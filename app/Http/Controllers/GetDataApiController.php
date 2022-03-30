b265adf0623dd175c5b17d80a322f368f9b2c4f2
Jean Freitas <henriquek3@live.com>
2022-03-30 10:32:11 -0400

    Refatoração
    Deixando dd no código para comparação de ambiente e estudo de caso
    
    Took 55 seconds

--- app/Http/Controllers/GetDataApiController.php
+++ app/Http/Controllers/GetDataApiController.php
@@ -2,68 +2,75 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use App\Services\ComunicaService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 class GetDataApiController extends Controller
 {
-   public function index()
-   {
+    public function __construct()
+    {
+        ini_set('max_execution_time', 180); //3 minutes
+    }
+
+    public function index()
+    {
         $response = Http::get('https://www.jacto.com/api/v1/products?market=1');
         //$response = Http::get('https://linsbrasil.com.br/wp-json/wp/v2/posts');
         //$response = Http::get('https://agromaq.agr.br/wp-json/wp/v2/posts');
-        if($response){
-            ini_set('max_execution_time', 180); //3 minutes
+        if ($response) {
             foreach ($response->json() as $rs) {
-                //dd($rs); exit;
                 $rs = (object)$rs;
-                $title = $rs->name['pt_BR'];
+                if ($rs->id === 247) {
+                    dd($rs);
+                    $rs = (object)$rs;
+                    $title = $rs->name['pt_BR'];
 
-                //Pega as imagens
+                    //Pega as imagens
                     $result = count($rs->gallery);
                     $fotos = "";
-                    for($i=0; $i < $result; $i++){
+                    for ($i = 0; $i < $result; $i++) {
                         $fotos .= "<div style='margin: 5px; border: 1px solid #ccc;float: left; width: 214px; height:214px; overflow:hidden;'>";
-                        $fotos .= "<img style = 'width:100%;height:auto;' src='".$rs->gallery[$i]['url']."' />";
+                        $fotos .= "<img style = 'width:100%;height:auto;' src='" . $rs->gallery[$i]['url'] . "' />";
                         $fotos .= "</div><br>";
                     }
 
-                //Pega os itens
-                         $resultB = count($rs->features);
-                         $features = "<h3 style='color:orange;'>DIFERENCIAIS</H3><br>";
+                    //Pega os itens
+                    $resultB = count($rs->features);
+                    $features = "<h3 style='color:orange;'>DIFERENCIAIS</H3><br>";
                     $features .= "<ul>";
-                    for($i=0; $i < $resultB; $i++){
+                    for ($i = 0; $i < $resultB; $i++) {
                         $features .= "<li>";
-                        $features .= "<h5 class='texte-uppercase'>".$rs->features[$i]['title']['pt_BR']."</h5>";
-                        $features .= "<p>".$rs->features[$i]['description']['pt_BR']."</p>";
+                        $features .= "<h5 class='texte-uppercase'>" . $rs->features[$i]['title']['pt_BR'] . "</h5>";
+                        $features .= "<p>" . $rs->features[$i]['description']['pt_BR'] . "</p>";
                         $features .= "</li>";
 
                     }
                     $features .= "</ul>";
 
-                //Adiciona as imagens e os itens no corpo da descrição (content)
+                    //Adiciona as imagens e os itens no corpo da descrição (content)
 
-                $content = "<div style='margin: 5px 15px 5px 5px; border: 1px solid #ccc;float: left; width: 280px; height:214px; overflow:hidden;'>";
-                $content .= "<img width='214' height='45' src='".$rs->image['url']."' /></div>";
-                $content .= "<p>".$rs->description['pt_BR']."</p><figure><img width='214' height='200' src='".$rs->image['url']."' alt='' class='wp-image-740' srcset='https://agromaq.agr.br/wp-content/uploads/2022/02/image-1.png 214w, https://agromaq.agr.br/wp-content/uploads/2022/02/image-1-120x25.png 120w' sizes='(max-width: 214px) 100vw, 214px' /></figure>.<br>".$fotos."<br><br><br>".$features;
-
-
+                    $content = "<div style='margin: 5px 15px 5px 5px; border: 1px solid #ccc;float: left; width: 280px; height:214px; overflow:hidden;'>";
+                    $content .= "<img width='214' height='45' src='" . $rs->image['url'] . "' /></div>";
+                    $content .= "<p>" . $rs->description['pt_BR'] . "</p><figure><img width='214' height='200' src='" . $rs->image['url'] . "' alt='' class='wp-image-740' srcset='https://agromaq.agr.br/wp-content/uploads/2022/02/image-1.png 214w, https://agromaq.agr.br/wp-content/uploads/2022/02/image-1-120x25.png 120w' sizes='(max-width: 214px) 100vw, 214px' /></figure>.<br>" . $fotos . "<br><br><br>" . $features;
 
 
-                $slug = $rs->slug['pt_BR'];
-                $dados = ComunicaService::enviarDados($title, $content, $slug);
-                //log($title);
-                
+                    $slug = $rs->slug['pt_BR'];
+                    $dados = ComunicaService::enviarDados($title, $content, $slug);
+                    Log::notice($slug);
+                }
+
             }
-        }else{
-            echo"Não há dados";
+        } else {
+            echo "Não há dados";
         }
     }
+
     public function atualizar($id, $title, $content, $slug)
     {
         $dados = ComunicaService::atualizarDados($id, $title, $content, $slug);
     }
+
     public function excluir($id)
     {
         $dados = ComunicaService::excluirDados($id);
