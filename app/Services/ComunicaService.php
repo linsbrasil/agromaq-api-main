533bd6fe76eb97d34c7fbb3d38b8dca5f1dca3b5
Jean Freitas <henriquek3@live.com>
2022-03-30 10:31:17 -0400

    rearranjo de código
    Took 20 seconds

--- app/Services/ComunicaService.php
+++ app/Services/ComunicaService.php
@@ -1,55 +1,47 @@
 <?php
+
 namespace App\Services;
+
 use Illuminate\Support\Facades\Http;
 
 class ComunicaService
 {
-	public static function enviarDados($title = null, $content = null, $slug = null)
-	{
-		//https://agromaq.agr.br/wp-json/wp/v2/posts
-		return  Http::withHeaders([
-			'Authorization' => 'Basic d2VibWluc3RlcjppVWlHIHYyTEMgMDhjUyBFYjUzIDl4WnggcHk0eA=='
-		])->post('https://agromaq.webminster.app/wp-json/wp/v2/posts', [
-			'title' => $title,
-			'content' => $content,
-			'slug' => $slug,
-			'publish' => '1',
-			'status' => 'publish'
-		]);
+    public static function enviarDados($title = null, $content = null, $slug = null)
+    {
+        //https://agromaq.agr.br/wp-json/wp/v2/posts
+        return Http::withHeaders([
+            //'Authorization' => 'Basic d2VibWluc3RlcjppVWlHIHYyTEMgMDhjUyBFYjUzIDl4WnggcHk0eA=='
+            'Authorization' => 'Basic ' . base64_encode(env('WP_API_USER') . ':' . env('WP_API_PW')), // Ambiente Webminster
+        ])->post('https://agromaq.webminster.app/wp-json/wp/v2/posts', [
+            'title' => $title,
+            'content' => $content,
+            'slug' => $slug,
+            'publish' => '1',
+            'status' => 'publish',
+        ]);
 
-	}
-	public static function atualizarDados($id, $title = null, $content = null, $slug = null)
-	{
-		return  Http::withHeaders([
-			'Authorization' => 'Basic bGluc2JyYXNpbDpLVVpzIDNmQmQgZGd6VSBKT29ZIFFSNTEgQUhxbQ=='
-		])->post('https://www.linsbrasil.com.br/wp-json/wp/v2/posts/'.$id, [
-			'title' => $title,
-			'content' => $content,
-			'slug' => $slug,
-			'publish' => '1',
-			'status' => 'publish'
-		]);
+    }
 
-	}
-	public static function excluirDados($id)
-	{
-		return  Http::withHeaders([
-			'Authorization' => 'Basic bGluc2JyYXNpbDpLVVpzIDNmQmQgZGd6VSBKT29ZIFFSNTEgQUhxbQ=='
-		])->delete('https://www.linsbrasil.com.br/wp-json/wp/v2/posts/'.$id);
+    public static function atualizarDados($id, $title = null, $content = null, $slug = null)
+    {
+        return Http::withHeaders([
+            'Authorization' => 'Basic bGluc2JyYXNpbDpLVVpzIDNmQmQgZGd6VSBKT29ZIFFSNTEgQUhxbQ==',
+        ])->post('https://www.linsbrasil.com.br/wp-json/wp/v2/posts/' . $id, [
+            'title' => $title,
+            'content' => $content,
+            'slug' => $slug,
+            'publish' => '1',
+            'status' => 'publish',
+        ]);
 
-	}
+    }
+
+    public static function excluirDados($id)
+    {
+        return Http::withHeaders([
+            'Authorization' => 'Basic bGluc2JyYXNpbDpLVVpzIDNmQmQgZGd6VSBKT29ZIFFSNTEgQUhxbQ==',
+        ])->delete('https://www.linsbrasil.com.br/wp-json/wp/v2/posts/' . $id);
+
+    }
 
 }
-
-
-//Logo abaixo estão os principais parametros a serem preenchidos
-/*{
-  "title" : "Mesa de madeira",
-  "slug": "mesa-madeira",
-  "published": 1,
-  'status' => 'publish',
-  "featured": 1,
-  "author": 1,
-  "categories": [3],
-  "content": "Empresa deste segmento produz madeira de excelênte qualidade"
-}*/
