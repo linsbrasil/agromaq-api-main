f9c7ffb28275ff21962c8503fa9042e3257ebf5d
Jean Freitas <henriquek3@live.com>
2022-04-06 14:20:34 -0400

    refatoração e add author
    Took 13 seconds

--- app/Services/ComunicaService.php
+++ app/Services/ComunicaService.php
@@ -9,28 +9,30 @@ class ComunicaService
     public static function enviarDados($title = null, $content = null, $slug = null)
     {
         return Http::withHeaders([
-                'Authorization' => 'Basic ' . base64_encode(env('WP_API_USER') . ':' . env('WP_API_PW')), 
-                /*Ambiente Webminster*/
-            ])->post(env('WP_API_DESTINY_URL'), [
-                'title' => $title,
-                'content' => $content,
-                'slug' => $slug,
-                'publish' => '1',
-                'status' => 'publish',
-            ]);
-
-        }
-
-        public static function atualizarDados($id, $title = null, $content = null)
-        {
-            return Http::withHeaders([
-            'Authorization' => 'Basic ' . base64_encode(env('WP_API_USER') . ':' . env('WP_API_PW')), 
+            'Authorization' => 'Basic ' . base64_encode(env('WP_API_USER') . ':' . env('WP_API_PW')),
             /*Ambiente Webminster*/
-        ])->post(env('WP_API_DESTINY_URL') .'/'. $id, [
+        ])->post(env('WP_API_DESTINY_URL'), [
+            'title' => $title,
+            'content' => $content,
+            'slug' => $slug,
+            'publish' => '1',
+            'status' => 'publish',
+            'author' => 2,
+        ]);
+
+    }
+
+    public static function atualizarDados($id, $title = null, $content = null)
+    {
+        return Http::withHeaders([
+            'Authorization' => 'Basic ' . base64_encode(env('WP_API_USER') . ':' . env('WP_API_PW')),
+            /*Ambiente Webminster*/
+        ])->post(env('WP_API_DESTINY_URL') . '/' . $id, [
             'title' => $title,
             'content' => $content,
             'publish' => '1',
             'status' => 'publish',
+            'author' => 2,
         ]);
 
     }
